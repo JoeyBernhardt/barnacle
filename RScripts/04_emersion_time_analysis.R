421bdf91271591db94f8040f8882b7c11d46d316
Joey Bernhardt <joey.bernhardt@biodiversity.ubc.ca>
2018-04-26 13:26:15 -0700

    writing out all ibutton data

--- RScripts/04_emersion_time_analysis.R
+++ RScripts/04_emersion_time_analysis.R
@@ -30,15 +30,42 @@ emersion_noBoulder %>%
 
 
 emersion_noBoulder %>% 
-	ggplot(data = ., aes(x = substrate, y = emersion_time_hours, fill = factor(substrate))) + geom_bar(stat = "identity") + 
+	ggplot(aes(x = substrate, y = emersion_time_hours, fill = factor(substrate))) + geom_bar(stat = "identity") + 
 	ylab("hours of emersion time per day") +
 	theme(axis.text=element_text(size=16),
 				axis.title=element_text(size=14,face="bold")) + facet_wrap( ~ Site) +
 	scale_fill_viridis(discrete = TRUE, begin = 0.7, end = 0.9)
 
 
+emersion_noBoulder %>% 
+	ggplot(aes(x = substrate, y = emersion_time_hours, color = factor(substrate))) + geom_point() + 
+	ylab("hours of emersion time per day") +
+	theme(axis.text=element_text(size=16),
+				axis.title=element_text(size=14,face="bold")) +
+	# facet_wrap( ~ Site) +
+	scale_color_viridis(discrete = TRUE, begin = 0.7, end = 0.9)
 
 mod1 <- lm(emersion_time_hours ~ substrate + Site, data = emersion_noBoulder)
 summary(mod1)
 
 levels(emersion_noBoulder$Region)
+unique(emersion_noBoulder$Site)
+
+
+
+em <- emersion_noBoulder %>% 
+	mutate(isite = NA) %>% 
+	mutate(isite = ifelse(Site == "CrabInnTofino", "Crab", isite)) %>% 
+	mutate(isite = ifelse(Site == "MotelTofino", "Sketchy", isite)) %>% 
+	mutate(isite = ifelse(str_detect(Site, "Toquart"), "Toquart", isite)) %>%
+	mutate(isite = ifelse(grepl("Eagle", Site), "Eagle", isite)) %>% 
+	mutate(isite = ifelse(grepl("Sheepfarm", Site), "Sheepfarm", isite)) %>% 
+	mutate(isite = ifelse(grepl("Uk", Site), "Ukie", isite)) %>% 
+	mutate(isite = ifelse(grepl("Whyte", Site), "Whyte", isite)) %>%
+	mutate(isite = ifelse(grepl("Sooke", Site), "Sooke", isite)) %>% 
+	mutate(isite = ifelse(grepl("28th", Site), "28th", isite)) %>% 
+	mutate(isite = ifelse(grepl("Caulfield", Site), "Caulfield", isite)) %>% 
+	mutate(isite = ifelse(grepl("Welbury", Site), "Welbury", isite)) %>% 
+	mutate(isite = ifelse(grepl("Copper", Site), "Copper", isite))
+	
+all4 <- read_csv("data-processed/all_ibutton_data.csv")
--- RScripts/05_ibutton_data.R
+++ RScripts/05_ibutton_data.R
@@ -2,9 +2,10 @@
 library(tidyverse)
 library(stringr)
 library(lubridate)
+library(purrr)
 
 
-i1 <- read_lines("data-raw/2012_Regional_ibutton_data/Outer_coast/Crab bench 2BA532.rtf", skip = 7)
+i1 <- read_lines("data-raw/2012_Regional_ibutton_data/Outer_coast/Crab bench 2BA532.rtf", skip = 7, n_max = 1720)
 i2 <- as.data.frame(i1) %>% 
 	separate(i1, into = c("date", "C", "temperature"), sep = ",") %>% 
 	mutate(temperature = str_replace(temperature, "\\\\", "")) %>% 
@@ -13,3 +14,70 @@ i2 <- as.data.frame(i1) %>%
 
 i2 %>% 
 	ggplot(aes(x = date, y = temperature)) + geom_point() + geom_line()
+
+
+### now read in all the ibutton files together
+
+i_files <- c(list.files("data-raw/2012_Regional_ibutton_data/Outer_coast", full.names = TRUE),
+						 list.files("data-raw/2012_Regional_ibutton_data/Saltspring", full.names = TRUE),
+						 list.files("data-raw/2012_Regional_ibutton_data/Vancouver", full.names = TRUE))
+						 
+
+names(i_files) <- i_files %>% 
+	gsub(pattern = ".rtf$", replacement = "")
+
+?read_lines()
+
+all_ibuttons <- map(i_files, read_lines, skip = 7, n_max = 1770)
+
+str(all_ibuttons)
+
+allb <- as.data.frame(all_ibuttons) %>% 
+	gather(value = "record", key = "site")
+
+allb2 <- allb %>% 
+	separate(record, into = c("date", "C", "temperature"), sep = ",") %>% 
+	mutate(temperature = str_replace(temperature, "\\\\", "")) %>% 
+	mutate(date = mdy_hms(date)) %>% 
+	mutate(temperature = as.numeric(temperature))
+
+allb3 <- allb2 %>% 
+	mutate(site = str_replace(site, "data.raw.2012_Regional_ibutton_data.", "")) %>%
+	mutate(site = str_replace(site, "_", "")) %>% 
+	separate(site, into = c("region", "Site", "substrate", "ibutton_id"), remove = FALSE)
+
+unique(allb3$substrate)
+
+allb3 %>% 
+	filter(substrate == "2") %>% View
+
+
+all4 <- allb3 %>% 
+	mutate(substrate = ifelse(substrate == "2", "bench", substrate)) %>%
+	mutate(substrate = ifelse(substrate == "1", "bench", substrate)) %>% 
+	mutate(substrate = ifelse(substrate == "B", "bench", substrate)) %>% 
+	mutate(substrate = ifelse(substrate == "Bench", "bench", substrate)) %>% 
+	mutate(substrate = ifelse(substrate == "Cobble", "cobble", substrate)) %>% 
+	select(-C) %>% 
+	mutate(Site = ifelse(Site == "Ukee", "Ukie", Site)) %>% 
+	mutate(Site = ifelse(Site == "UkieB", "Ukie", Site)) %>% 
+	mutate(Site = ifelse(Site == "eaglebench", "Eagle", Site)) %>% 
+	filter(date < "2012-09-01")
+	
+
+all4 %>% 
+	filter(ibutton_id == "2B9349") %>% 
+	filter(date > "2012-07-01", date < "2012-08-01") %>% 
+	ggplot(aes(x = date, y = temperature, color = region)) + geom_point() +
+	geom_line() +
+	facet_wrap( ~ ibutton_id)
+
+unique(all4$Site)
+unique(all4$ibutton_id)
+max(all4$date)
+
+all4 %>% 
+	filter(ibutton_id == "csv") %>% View
+
+write_csv(all4, "data-processed/all_ibutton_data.csv")
+	
